09600245d5e1cf68510bec9fa3b097b9aa191383
hthemir <hthemir@hotmail.com>
2017-05-30 22:01:01 -0300

    capitulo6

---
+++ capitulo6/texto/util/ImportarTexto.java
@@ -0,0 +1,21 @@
+package texto.util;
+
+import javax.swing.JOptionPane;
+
+public class ImportarTexto {
+	/** 
+	*@param endereco do arquivo
+	*@return arquivo
+	*/
+	public String importarArquivo(String endereco) {
+		return "";
+	}
+	/** 
+	*@param dados de entrada
+	*/
+	public void filtrarDados(String dados) {}
+
+	public static void main(String[] parametros) {
+		javax.swing.JOptionPane.showMessageDialog(null, "Teste de arquivo JAR");
+	}
+}
